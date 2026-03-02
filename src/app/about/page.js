9dd296ec8c8bf52a607b9fc118e1d3433db93b9a
unknown <devliyalhimanshu@gmsil.com>
2026-03-02 10:58:47 +0530

    about meta remove

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,17 +1,6 @@
 import { Breadcrumb } from "@/components/breadcrumb";
 import About from "./_componets/about";
 import Image from "next/image";
-export const metadata = {
-  title: "Non-ATL Technology Learning Kits – BDS Education",
-  description:
-    "Browse Non-ATL educational kits from BDS Education for early-stage technology learning in robotics, coding, and creative engineering projects.",
-  keywords: [
-    "non ATL kits",
-    "robotics education kits",
-    "coding kits for kids",
-    "tech learning components",
-  ],
-};
 
 export default function Aboutus(params) {
   return (
